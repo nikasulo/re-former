62d86aaf53f77caf421a1954135060ad2422c5f0
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-25 17:28:38 +0100

    Correct tab errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,9 +28,7 @@ class UsersController < ApplicationController
   end
 end
 
-
 private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
-  
+def user_params
+  params.require(:user).permit(:username, :email, :password)
+end
