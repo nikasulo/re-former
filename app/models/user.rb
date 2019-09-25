90bac3a29341db74b99ab1c9751d73ecd7e6c5b5
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-25 15:26:03 +0100

    Correct user model with password instead of name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :email, presence: true
-    validates :name, presence: true
+    validates :password, presence: true
     validates :username, presence: true
 end
